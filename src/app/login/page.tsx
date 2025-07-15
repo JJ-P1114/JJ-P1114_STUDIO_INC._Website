355dfe0deb5a5544fd4ce5c5145f84a954e98dc0
Jérémy Pichette <jj-p1114@outlook.com>
2025-07-14 21:52:31 -0400

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50">
+    <div className="flex justify-center items-center min-h-screen bg-white">
       <SignIn path="/login" routing="path" signUpUrl="/sign-up" />
     </div>
   );
